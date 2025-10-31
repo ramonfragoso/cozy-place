ff57965b73f10db40b35358b2670c713d2b66fb9
ramon38 <ramon@fetch.ly>
2025-10-31 02:25:22 -0300

    Change next config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: ['raw-loader']
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
